9f83acffd77da1313da71606af4b43e9557b8a00
Kirian Anglès <kirianag@gmail.com>
2017-11-28 12:38:31 +0100

    Create workspace with pods

---
+++ RandomUsers/RandomUsers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RandomUsers
+//
+//  Created by Kirian Anglès on 28/11/17.
+//  Copyright © 2017 Kirian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
