d2c9657b5d202878d32628a45d07628d86eb8940
Kirian Anglès <kirianag@gmail.com>
2017-11-30 12:22:07 +0100

    Retrieve more random users when user scroll at the end of the list

--- RandomUsers/RandomUsers/User List/UserListViewController.swift
+++ RandomUsers/RandomUsers/User List/UserListViewController.swift
@@ -12,6 +12,7 @@ class UserListViewController: UIViewController {
     @IBOutlet fileprivate weak var collectionView: UICollectionView!
     fileprivate let dataSource: CollectionViewDataSource<UserListAdapter>
     fileprivate var presenter: UserListPresenterType
+    fileprivate let searchBar: UISearchBar
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -21,37 +22,75 @@ class UserListViewController: UIViewController {
          dataSource: CollectionViewDataSource<UserListAdapter>) {
         self.presenter = presenter
         self.dataSource = dataSource
+
+        searchBar = UISearchBar()
+        searchBar.searchBarStyle = .minimal
+
         super.init(nibName: nil, bundle: nil)
         self.presenter.view = self
+        searchBar.delegate = self
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        presenter.viewDidLoad()
+        navigationItem.titleView = searchBar
         setupCollectionView()
+        presenter.viewDidLoad()
     }
     
     private func setupCollectionView() {
-        collectionView.rand_registerCell(UserListCollectionViewCell.self)
         collectionView.dataSource = dataSource
         collectionView.delegate = self
+        collectionView.rand_registerCell(UserListCollectionViewCell.self)
     }
 }
 
 extension UserListViewController: UserListView {
     func showLoadingView() {}
     func hideLoadingView() {}
+    
     func show(users: [UserEntity]) {
         dataSource.updateSource(sectionsData: users)
         collectionView.reloadData()
+    }
+    
+    func scrollToItem(at index: Int) {
+        let indexPath = IndexPath(item: index, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: .bottom, animated: true)
     }
 }
 
 extension UserListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-        
         presenter.didTapCell(at: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        presenter.willDisplayItem(at: indexPath.row, itemCount: dataSource.adapter.itemCount())
+    }
+}
+
+extension UserListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        presenter.filterUsers(by: searchText)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(false, animated: true)
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        
+        return true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = nil
+        searchBar.setShowsCancelButton(false, animated: true)
+        searchBar.resignFirstResponder()
+        presenter.didTapSearchCancel()
     }
 }
